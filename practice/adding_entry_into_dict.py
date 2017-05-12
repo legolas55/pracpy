a84d4fe8692f6412c98e2b0e25c8ebb499ff628d
tianyichen0 <tianyichen0@gmail.com>
2017-05-12 14:31:54 -0400

    adding values to a dictionary
    different ways to do the same thing

---
+++ practice/adding_entry_into_dict.py
@@ -0,0 +1,67 @@
+#add an entry into a dictionary
+
+#look before you leap
+import collections 
+
+index={}
+
+
+def addEntry(word,param):
+    if index.has_key(word):
+        index[word].append(param)
+    else :
+        index[word]=[param]
+
+addEntry("red",12)
+
+print index
+
+addEntry("yellow",14)
+
+print index
+
+addEntry("red",18)
+
+print index
+
+
+#easier to get permisson
+
+index={}
+
+def addEntryPerm(word,param):
+    try:index[word].append(param)
+    except KeyError: index[word]=[param]
+
+addEntryPerm("sand",123)
+print index
+
+addEntryPerm("sand",1234)
+print index
+
+
+#setdefault
+index={}
+
+def addword(word,param):
+    index.setdefault(word,[]).append(param)
+
+addword("sandy",777)
+print index
+
+addword("sandy",888)
+print index
+
+#even simpler
+data={("one",7),("one",8)}
+index=collections.defaultdict(list)
+
+for (key,value) in data:
+    index[key].append(value)
+
+print index
+
+
+
+
+
