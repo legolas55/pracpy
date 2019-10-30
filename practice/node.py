5aceeee2e62a8a3eb33530d2acb9406f7745bb39
tchen <tchen@dispersivegroup.com>
2019-10-30 01:39:56 -0400

    Update node.py
    Somewhat working delete function

--- practice/node.py
+++ practice/node.py
@@ -1,8 +1,20 @@
-""" TO DO
+""" Write a method that answers the following problem:
+Accept as input:
+·       list: a singly-linked list
+Remove the middle element of the list without iterating the list more than once.
+Assume the list size cannot be known until traversed.
+Support your answer with tests.
 """
 
+### Assumptions
+# 1.
+# 2.
 
-# Data Structures
+
+### Data Structures
+# Would improve on data structure choice on the next iteration.
+# Array lists seem to be faster than linked list with Nodes.
+# Every time a node is created, 
 
 class Node():
     """ Node Class
@@ -45,9 +57,25 @@ class LinkedList():
         new_node.set_next(self.head)
         self.head = new_node
 
-    def delete_node(self):
-        """Deletes a node """
-
+    def delete_node(self, data):
+        """Deletes a node by looking at self.data value"""
+        current_node=self.head
+        previous_node=None
+        found_node = False
+        while current_node and found_node is False:
+            if current_node.get_data() == data:
+                found_node = True
+            else:
+                previous_node = current_node
+                current_node = current_node.get_next()
+            if current_node is None:
+                raise ValueError("Data not in list")
+            if previous_node is None:
+                self.head=current_node.get_next()
+            else:
+                previous_node.set_next(current_node.get_next())
+        
+        
 
 # Helper methods for testing
 
@@ -80,52 +108,86 @@ def print_linked_list(linked_list):
 
 
 # Delete Use case for testing
+# FUT- Function Under Test
 def delete_middle_node_from_linked_list(linked_list):
     """This function deletes the middle node from a singley LinkedList.
-        Args: linked_list- LinkedList object to be passed in.
+        Args: linked_list - LinkedList object to be passed in.
         Returns: Nothing
     """
     sizeoflist = 0
     middle = linked_list.head
 
     if middle is None:
-        print(
-            "Size of list is {0}. No Middle node to delete".format(sizeoflist))
-
-    # elif(middle.next_node==None):
-    #    sizeoflist=sizeoflist+1
-    #    print("Size of Linked List is ", sizeoflist)
-    #    print("Deleting Middle Node which is ", middle.data)
-
+        print("Size of list is {0}. No Middle node to delete"
+              .format(sizeoflist))
     else:
         one_step = linked_list.head
         two_step = linked_list.head
-        print("add 1")
-        # sizeoflist=sizeoflist+1
+
+        #Since there is a head node, size must be at least 1
+        sizeoflist=sizeoflist+1
+        #print("add 1")
+    
+        #The condition is structured like this to avoid a NoneType error for two_step
+        #This could be written as while(true) and except a NoneType error, but not recommended
+        #Look before you leap is better in this situation
         while two_step.next_node is not None and two_step.next_node.next_node is not None:
             one_step = one_step.get_next()
             sizeoflist = sizeoflist + 1
-            print("add 1.5")
-            # print(one_step.data)
+            #print("add 1.5")
+            #print(one_step.data)
             two_step = two_step.get_next().get_next()
             sizeoflist = sizeoflist + 1
-            print("add 2")
+            #print("add 2")
 
+        #Two_step pointing at a node with a next node but no next next node
+        #Since calculating size of list, need edge case for correct size.
+        if two_step.next_node is not None:
+            sizeoflist=sizeoflist+1
+
+        #This is the middle node
         middle = one_step
+        
+        print("Size of Linked List is ", sizeoflist)
+        print("Deleting Middle Node which is ", middle.data,"\n")
 
-        #print("Size of Linked List is ", sizeoflist)
-        print("Deleting Middle Node which is ", middle.data)
+        linked_list.delete_node(middle.data)
+
+        print("\n")
+        print_linked_list(linked_list)
+        print("\n")
+        
+        return linked_list
 
 
-GENERATED_LIST = generate_linked_list_append_front(5)
-# print(generated_list)
+#This could be made into a decorator
+def test_delete_middle_node(size_list):
+    """ This function will generate a singly linked list of size size_list,
+        print the list and test the delete_middle_node_from_linked_list function.
+    """ 
+    GENERATED_LIST = generate_linked_list_append_front(size_list)
+    print_linked_list(GENERATED_LIST)
+    delete_middle_node_from_linked_list(GENERATED_LIST)
+    
 
-print_linked_list(GENERATED_LIST)
+#Unit Tests
 
-print("")
-delete_middle_node_from_linked_list(GENERATED_LIST)
+# Case Null List 
+test_delete_middle_node(0)
 
-# generated_list=generate_linked_list_append_front(0)
-# print_linked_list(generated_list)
+#Case Only Head Node
+test_delete_middle_node(1)
 
-# delete_middle_node_from_linked_list(generated_list)
+#Case Two Nodes not triggering the loop, Test size edge case.
+test_delete_middle_node(2)
+
+#Case Test Loop
+test_delete_middle_node(3)
+
+#Keep going
+test_delete_middle_node(6)
+
+
+
+
+
